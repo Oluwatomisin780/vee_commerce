cf197b9b8e362886189af53ce2e1c506aea1263f
oluwatomisin780 <tomisinoyediran@gmail.com>
2023-10-27 19:58:46 +0100

    seed database

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { hostname } from 'os';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
